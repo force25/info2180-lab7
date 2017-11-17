a1c1db5bc082f130d0c43f58e28291f6182d0054
Jonathan Wilberforce <jonforce52@hotmail.com>
2017-11-17 19:58:03 +0000

    exercise 4

--- world.js
+++ world.js
@@ -1,13 +1,20 @@
 window.onload = function(){
 var button = document.getElementById("lookup");
+var c_box = document.getElementById("checkbox");
+var country = document.getElementById("country");
+
 button.addEventListener('click',function(){
     var http = new XMLHttpRequest();
        http.onreadystatechange = function() {
-      if (http.readyState === 4 && http.status === 200) {
-      alert(document.getElementById("result").innerHTML="<h3>Result</h3>"+http.responseText);
+      if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+      document.getElementById("result").innerHTML="<h3>Result</h3>"+http.responseText;
         }
     };
-     http.open("GET", "world.php?q=",true);
-     http.send();
-})
+    if(c_box.checked){
+           http.open("GET","world.php?all=true",true);
+       }else{
+           http.open("GET","world.php?country="+country.value,true);
+       }
+       http.send();
+});
 }
--- world.php
+++ world.php
@@ -1,7 +1,7 @@
 <?php
 
 $get_request = $_GET['all'];
-$country= $_GET['country']="Jamaica";
+$country= $_GET['country'];
 $host = getenv('IP');
 $username = getenv('C9_USER');
 $password = '';
@@ -9,10 +9,14 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+if($get_request){
+    $stmt = $conn->query("SELECT * FROM countries");
+}
+elseif($country!=''){
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+}
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 echo '<ul>';
 foreach ($results as $row) {
   echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
